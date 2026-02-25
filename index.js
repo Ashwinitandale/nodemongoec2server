8911a06fe79a1e2f251646d4ae61ece6703b2ac4
Ashwinitandale <80951217+Ashwinitandale@users.noreply.github.com>
2026-02-25 14:47:25 +0530

    port

--- index.js
+++ index.js
@@ -94,6 +94,6 @@ app.get('/fetchData', async (req, res) => {
 //     res.json(`ID deleted succesfully ${deleteID}`);
 // })
 
-app.listen(3000, () => {
-    console.log("listneing post 3000");
-})
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on port 3000");
+});
